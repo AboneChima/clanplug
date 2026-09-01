c3c173fdfb9464fcc1a4fa3ca09b8822a3295898
Celebspotlight <abonejoseph@gmail.com>
2026-09-01 13:58:52 +0100

    Lower feed content more: 60px overlay, 90px buttons

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -766,7 +766,7 @@ function FeedContent() {
                 {/* Bottom Overlay - User Info & Description */}
                 <div 
                   className="absolute left-0 right-0 px-4 pb-2 pointer-events-none z-10 feed-bottom-overlay" 
-                  style={{ bottom: '100px' }}
+                  style={{ bottom: '60px' }}
                 >
                   <div className="pointer-events-auto max-w-xl">
                     {/* Description - Only show for media posts */}
@@ -831,7 +831,7 @@ function FeedContent() {
                 {/* Right Side - Action Buttons - Positioned higher */}
 <div 
                   className="absolute right-3 flex flex-col gap-6 z-10 feed-action-buttons" 
-                  style={{ bottom: '120px' }}
+                  style={{ bottom: '90px' }}
                 >
                   {/* Like */}
                   <button
